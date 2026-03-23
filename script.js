8674a21a748c14591bf57a935d1ebaca49e742f2
matteojose3112-bit <matteo.jose3112@gmail.com>
2026-03-23 16:41:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+const balance = document.getElementById("balance");
+const form = document.getElementById("form");
+const list = document.getElementById("list");
+const incomeEl = document.getElementById("income");
+const expenseEl = document.getElementById("expense");
+
+let currentFilter = "all";
+
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+function updateUI() {
+  list.innerHTML = "";
+
+  let total = 0;
+  let income = 0;
+  let expense = 0;
+
+  transactions.forEach((t, index) => {
+
+  if (currentFilter === "income" && t.amount < 0) return;
+  if (currentFilter === "expense" && t.amount > 0) return;
+
+    if (t.amount > 0) income += t.amount;
+    else expense += t.amount;
+
+    const li = document.createElement("li");
+    li.classList.add(t.amount > 0 ? "income" : "expense");
+
+    li.innerHTML = `
+  <div>
+    ${t.text}
+    <span class="tag ${t.category.toLowerCase()}">${t.category}</span>
+  </div>
+  <span>${t.amount > 0 ? "+" : ""}$${Math.abs(t.amount)}</span>
+  <button onclick="deleteTx(${index})">x</button>
+`;
+
+    list.appendChild(li);
+  });
+
+  balance.innerText = total;
+  incomeEl.innerText = income;
+  expenseEl.innerText = expense;
+}
+
+function deleteTx(index) {
+  transactions.splice(index, 1);
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+  updateUI();
+}
+
+function setFilter(type) {
+  currentFilter = type;
+  updateUI();
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const text = document.getElementById("text").value;
+  const amount = +document.getElementById("amount").value;
+  const category = document.getElementById("category").value;
+
+  transactions.push({ text, amount, category });
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+
+  form.reset();
+  updateUI();
+});
+
+let chart;
+
+function updateChart() {
+  const data = {};
+
+  // group by category
+  transactions.forEach(t => {
+    if (t.amount < 0) {
+      if (!data[t.category]) data[t.category] = 0;
+      data[t.category] += Math.abs(t.amount);
+    }
+  });
+
+  const labels = Object.keys(data);
+  const values = Object.values(data);
+
+  // destroy old chart (important)
+  if (chart) chart.destroy();
+
+  const ctx = document.getElementById("chart").getContext("2d");
+
+  chart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: labels,
+      datasets: [{
+        data: values
+      }]
+    }
+  });
+}
+
+function clearAll() {
+  transactions = [];
+  localStorage.removeItem("transactions");
+  updateUI();
+}
+
+function exportData() {
+  const dataStr = JSON.stringify(transactions, null, 2);
+  const blob = new Blob([dataStr], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "transactions.json";
+  a.click();
+}
+
+if (transactions.length === 0) {
+  list.innerHTML = "<p>No transactions yet</p>";
+  balance.innerText = 0;
+  incomeEl.innerText = 0;
+  expenseEl.innerText = 0;
+  return;
+}
+
+updateUI();
+
+updateChart();
